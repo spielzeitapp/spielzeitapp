e5105a4e61567c0d4d47d02a6cf0e2150d5a06c1
Johannes Baumann <office@quetschnguru.at>
2026-03-06 15:34:34 +0100

    fix vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/spielzeitapp_test/",
+  base: "/",
   plugins: [react()],
   server: {
     host: true,
